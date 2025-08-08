29cd80214e879bb3735c5f855aa2843ea1f09852
miraz66 <mirajulislam0509@gmail.com>
2025-08-08 12:32:41 +0600

    Refactor sales components: remove unused code and update field mappings

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,6 +2,9 @@ export default function Test() {
   return (
       <div className="mt-10 mb-10 container mx-auto">
         helo my name is test
+        helo my name is test
+        helo my name is test
+        helo my name is test
       </div>
   );
 };
